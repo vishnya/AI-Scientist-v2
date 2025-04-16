9b49e3b31f9815bf6e6275e7258b2a5c88c93b46
Cong Lu <conglu97@outlook.com>
2025-04-16 16:15:52 +0100

    update gpt list

--- ai_scientist/llm.py
+++ ai_scientist/llm.py
@@ -13,15 +13,27 @@ MAX_NUM_TOKENS = 4096
 AVAILABLE_LLMS = [
     "claude-3-5-sonnet-20240620",
     "claude-3-5-sonnet-20241022",
+    # OpenAI models
+    "gpt-4o-mini",
     "gpt-4o-mini-2024-07-18",
+    "gpt-4o",
     "gpt-4o-2024-05-13",
     "gpt-4o-2024-08-06",
+    "gpt-4.1",
+    "gpt-4.1-2025-04-14",
+    "gpt-4.1-mini",
+    "gpt-4.1-mini-2025-04-14",
+    "o1",
     "o1-2024-12-17",
     "o1-preview-2024-09-12",
+    "o1-mini",
     "o1-mini-2024-09-12",
     "o3-mini",
+    "o3-mini-2025-01-31",
+    # DeepSeek Models
     "deepseek-coder-v2-0724",
     "deepcoder-14b",
+    # Llama 3 models
     "llama3.1-405b",
     # Anthropic Claude models via Amazon Bedrock
     "bedrock/anthropic.claude-3-sonnet-20240229-v1:0",
@@ -306,7 +318,7 @@ def get_response_from_llm(
                 content = response.json()["generated_text"]
             else:
                 raise ValueError(f"Error from HuggingFace API: {response.text}")
-        
+
         new_msg_history = new_msg_history + [{"role": "assistant", "content": content}]
     elif model in ["meta-llama/llama-3.1-405b-instruct", "llama-3-1-405b-instruct"]:
         new_msg_history = msg_history + [{"role": "user", "content": msg}]
